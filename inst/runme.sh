3bc1355cf233641212c5d5164a27888cafaab31e
Piotr Bazydło <piotr.bazydlo@gmail.com>
2014-07-10 14:55:23 +0200

    Fixed Lyx setup. Changed language to polish.

--- inst/runme.sh
+++ inst/runme.sh
@@ -2,3 +2,5 @@ sudo apt-get install vim -y
 sudo apt-get install lyx -y
 sudo apt-get install texlive-fonts-extra -y
 sudo cp ./dcs.module /usr/share/lyx/layouts/
+sudo apt-get install latex-cjk-all -y
+sudo aptitude install texlive-lang-polish -y
