b3db9913f85eefc473570c13cf336c669672a659
Zachariusz Karwacki <zachariusz@karwacki.at>
2014-07-11 18:51:17 +0200

    background, implementation

---
+++ souce/IRemoteExecutorService.cs
@@ -0,0 +1,18 @@
+﻿[ServiceContract]
+public interface IRemoteExecutorService
+{
+    [OperationContract]
+    Guid Initialize(byte[] methodHandle);
+
+    [OperationContract]
+    void Execute(Guid eId, object[] parameters, ServiceUri callbackUri);
+
+    [OperationContract]
+    void ExecuteCallback(Guid eid, RemoteExecutorServiceResult executeResult);
+
+    [OperationContract]
+    RemoteExecutorServiceResult TryJoin(Guid eId);
+
+    [OperationContract]
+    PerformanceStatistics GetPerformanceStatistics();
+}
