04ac9c3e0b56f7be5688ede6c698ac332094a327
Zachariusz Karwacki <zachariusz@karwacki.at>
2014-07-12 16:43:03 +0200

    concept, implementation

---
+++ souce/ICentralizedDiscoveryService.cs
@@ -0,0 +1,15 @@
+﻿[ServiceContract]
+public interface ICentralizedDiscoveryService
+{
+    [OperationContract]
+    ServiceUri[] GetAvailableServices();
+
+    [OperationContract]
+    void Register(ServiceUri uri);
+
+    [OperationContract]
+    void Unregister(ServiceUri uri);
+
+    [OperationContract]
+    Task<Dictionary<ServiceUri, PerformanceStatistics>> GetPerformanceStatistics();
+}
